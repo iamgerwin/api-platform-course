57266b4709fb026403008fb6b98faf80ac29c5c2
iamgerwin <iamgerwin@aim.com>
2020-07-30 17:11:34 +0800

    add api platform and user + comment entities

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 
 use App\Repository\BlogPostRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=BlogPostRepository::class)
+ * @ApiResource()
  */
 class BlogPost
 {
