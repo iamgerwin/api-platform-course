4bd08a3c7511c79c5f8796ba238a67b7687f991b
iamgerwin <iamgerwin@aim.com>
2020-07-30 18:41:07 +0800

    update fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\Comment;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,11 +12,20 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+        $this->loadComments($manager);
+        $this->loadBlogPosts($manager);
+    }
+    
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_mariz');
+        
         $blogPost = new BlogPost();
         $blogPost->setTitle("A first post!");
         $blogPost->setPublished(new \DateTime('2020-07-30 12:00:00'));
         $blogPost->setContent("Content here!");
-        $blogPost->setAuthor("Mariz test");
+        $blogPost->setAuthor($user);
         $blogPost->setSlug("a-first-post");
         $manager->persist($blogPost);
 
@@ -22,10 +33,46 @@ class AppFixtures extends Fixture
         $blogPost->setTitle("Second post!");
         $blogPost->setPublished(new \DateTime('2020-07-31 12:00:00'));
         $blogPost->setContent("Second Content here!");
-        $blogPost->setAuthor("Mariz test");
+        $blogPost->setAuthor($user);
         $blogPost->setSlug("second-post");
         $manager->persist($blogPost);
 
+        $manager->flush();
+    }
+    
+    public function loadComments(ObjectManager $manager)
+    {
+        $user = $this->getReference('commenter_test');
+        
+        $comment = new Comment();
+        $comment->setAuthor($user);
+        $comment->setContent("This is a comment.");
+        $comment->setPublished(new \DateTime('now'));
+        
+        $manager->persist($comment);
+        $manager->flush();
+    }
+    
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername("mariz");
+        $user->setPassword("password");
+        $user->setEmail("mariz@gmail.com");
+        $user->setName("Mariz Parayno");
+        
+        $this->addReference('user_mariz', $user);
+        $manager->persist($user);
+        
+        $commenter = new User();
+        $commenter->setUsername("commenter");
+        $commenter->setPassword("sercret");
+        $commenter->setEmail("commenter@gmail.com");
+        $commenter->setName("Commenter Test");
+        
+        $this->addReference('commenter_test', $commenter);
+        $manager->persist($commenter);
+        
         $manager->flush();
     }
 }
